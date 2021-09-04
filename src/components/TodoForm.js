0990502f682a886a6aaf45c14a0a4723453669f6
Igor Milosevic <igor.milosevic1988@gmail.com>
2021-09-04 11:18:41 +0200

    adds fetch post so now date and todo is sent to backend

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -4,7 +4,7 @@ const TodoForm = (props) => {
  const { date } = props
  // My state for the input textbox
  const [todo, setTodo] = useState('')
- const [todos, setTodos] = useState([])
+ const [todos, setTodos] = useState([]) // Create an array that will contain all the todo objects
 
 
  // Creating my handle when user clicks the save todo button
@@ -13,18 +13,31 @@ const TodoForm = (props) => {
   if (todo) {
    console.log('Skicka todo till db');
    const todoInput = { todo, date }
+
+   const url = 'https://reqres.in/api/posts'
+
+   const request = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(todoInput)
+   };
+
+   fetch(url, request)
+    .then(response => response.json())
+    .then(data => console.log(data, 'data RAD 27'));
+
    setTodos((todos) => {
     return [...todos, todoInput]
    })
    setTodo('')
-   console.log(todoInput);
   } else {
    alert('Fyll i en todo')
   }
-
  }
+
+
  return (
-  <>
+  <article>
    <form>
     <input
      type="text"
@@ -35,9 +48,7 @@ const TodoForm = (props) => {
      placeholder='Fyll i en todo...' />
    </form>
    <button type="submit" onClick={handleSubmit}>Spara todo</button>
-
-   {console.log(date, 'hej')}
-  </>
+  </article>
  )
 }
 
