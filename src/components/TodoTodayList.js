e81023782fcf3ba6468a8c28c260022e0d9276fc
Igor Milosevic <igor.milosevic1988@gmail.com>
2021-09-11 19:35:28 +0200

    when clicking on a specific date, all todos is listed for that specific day

--- src/components/TodoTodayList.js
+++ src/components/TodoTodayList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import OnClickDayTodos from './OnClickDayTodos'
 
 const TodoTodayList = ({ displayTodo }) => {
